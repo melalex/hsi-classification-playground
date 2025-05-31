496dda6328d5910850a902a2e035063e28c974e8
melalex <alexander.melashchenko@gmail.com>
2025-05-31 18:06:33 +0000

    PU multiview WIP

--- src/trainer/pu_multiview_trainer.py
+++ src/trainer/pu_multiview_trainer.py
@@ -1,11 +1,15 @@
 from collections import defaultdict
 from typing import Optional
 
-from torch import Tensor, nn
+from torch import Generator, Tensor, nn
 import torch
 from torch.utils import data
 
-from src.data.dataset_decorator import NegativeDataset, UnlabeledDatasetDecorator
+from src.data.dataset_decorator import (
+    NegativeDataset,
+    PuDatasetDecorator,
+    UnlabeledDatasetDecorator,
+)
 from src.model.ensemble import MultiViewEnsemble
 from src.trainer.base_trainer import (
     BaseTrainer,
@@ -18,17 +22,21 @@ from src.util.progress_bar import create_progress_bar
 from src.util.torch import dataloader_from_prtototype
 
 
-class MultiViewTrainer:
+class PuMultiViewTrainer:
 
     def __init__(
         self,
         num_classes: int,
+        batch_size: int,
         confidence_threshold: float,
+        generator: Generator,
         device: torch.device,
         max_epochs: Optional[int] = None,
         ensemble_confidence_threshold: float = 0.5,
     ):
+        self.batch_size = batch_size
         self.confidence_threshold = confidence_threshold
+        self.generator = generator
         self.support = ModelSupport(num_classes, device)
         self.max_epochs = max_epochs
         self.ensemble_confidence_threshold = ensemble_confidence_threshold
@@ -37,17 +45,12 @@ class MultiViewTrainer:
         self,
         models: list[TrainableModule],
         trainers: list[BaseTrainer],
-        labeled: list[data.DataLoader],
-        unlabeled: data.DataLoader,
-        model_eval_dl: Optional[list[data.DataLoader]] = None,
-        ensemble_eval_dl: Optional[data.DataLoader] = None,
+        x: Tensor,
+        y: Tensor,
+        eval_dl: Optional[data.DataLoader] = None,
     ) -> tuple[TrainerFeedback, nn.Module]:
-        assert len(models) == len(trainers) and len(models) == len(labeled)
-
         history = []
-        localy_labeled = labeled[:]
-        localy_unlabeled = unlabeled
-        model_eval_dl = model_eval_dl if model_eval_dl else [None] * len(models)
+        y_i = [torch.clone(y) for _ in range(len(models))]
 
         with create_progress_bar()(total=self.max_epochs) as pb:
             pb.set_postfix(unlabeled_len=len(localy_unlabeled.dataset))
@@ -61,7 +64,6 @@ class MultiViewTrainer:
                     models=models,
                     trainers=trainers,
                     labeled=localy_labeled,
-                    eval_dl=model_eval_dl,
                 )
 
                 localy_unlabeled, localy_labeled, early_stop = self.__pseudo_label_all(
@@ -71,7 +73,7 @@ class MultiViewTrainer:
                 )
 
                 t_metrics = {"unlabeled_len": len(localy_unlabeled.dataset)}
-                e_metrics = self.validate(MultiViewEnsemble(models), ensemble_eval_dl)
+                e_metrics = self.validate(MultiViewEnsemble(models), eval_dl)
                 h_entry = TrainerHistoryEntry(t_metrics, e_metrics)
 
                 history.append(h_entry)
@@ -101,42 +103,47 @@ class MultiViewTrainer:
         self,
         models: list[TrainableModule],
         trainers: list[BaseTrainer],
-        labeled: list[data.DataLoader],
-        eval_dl: list[data.DataLoader],
+        x: Tensor,
+        y_i: list[Tensor],
     ):
-        for model, trainer, dl, eval_dl in zip(models, trainers, labeled, eval_dl):
-            trainer.fit(model, dl, eval_dl)
+        for i, (model, trainer, y) in enumerate(zip(models, trainers, y_i)):
+            dl = data.DataLoader(
+                dataset=PuDatasetDecorator(data.TensorDataset(x, y), i),
+                batch_size=self.batch_size,
+                shuffle=False,
+                generator=self.generator,
+            )
+
+            trainer.fit(model, dl)
 
     def __pseudo_label(self, model: nn.Module, x: Tensor):
         y_pred = self.support.predict_batch(model, x)
         return y_pred > self.confidence_threshold
 
     def __pseudo_label_all(
-        self,
-        models: list[TrainableModule],
-        labeled: list[data.DataLoader],
-        unlabeled: data.DataLoader,
+        self, models: list[TrainableModule], x: Tensor, y_i: list[Tensor]
     ):
         models_count = len(models)
         new_labeled = defaultdict(list)
         new_unlabled = []
         labels_count_not_increased = True
 
-        for x in unlabeled:
-            high_confidence = [self.__pseudo_label(it, x) for it in models]
-            stacked_confidence = torch.stack(high_confidence)
-            only_one_confident = stacked_confidence.sum(dim=0) == 1
-            unconfident = ~only_one_confident
+        high_confidence = [self.__pseudo_label(it, x) for it in models]
+        stacked_confidence = torch.stack(high_confidence)
+        only_one_confident = stacked_confidence.sum(dim=0) == 1
+        unconfident = ~only_one_confident
 
-            new_unlabled.append(x[unconfident.cpu()])
+        new_unlabled.append(x[unconfident.cpu()])
 
-            for i, confident in enumerate(high_confidence):
-                mask = confident & only_one_confident
+        for i, confident in enumerate(high_confidence):
+            mask = confident & only_one_confident
 
-                for j in range(models_count):
-                    if j != i:
-                        new_labeled_x = x[mask.cpu()]
-                        new_labeled[j].append(NegativeDataset(new_labeled_x))
+            for j, y in enumerate(y_i):
+                unlabeled = 
+                if j != i:
+                    y[mask] = 0
+                else:
+                    y[mask] = 1
 
         new_labeled_dl = []
 
